6685d5da8837b01b4fdb237e9867c427f1e4db79
Juan Díaz <Daz@LAPTOP-12TBI1NP>
2021-10-08 09:48:48 -0300

    organizacion con carpetas

---
+++ Inquiries/Presentación/VerUsuAdmin.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace Inquiries
+{
+    partial class VerUsuAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblAlDoc = new System.Windows.Forms.Label();
+            this.lblCédula = new System.Windows.Forms.Label();
+            this.lblContra = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnGruposAl = new System.Windows.Forms.Button();
+            this.btnPerfilDoc = new System.Windows.Forms.Button();
+            this.btnConsultasAl = new System.Windows.Forms.Button();
+            this.btnChatAl = new System.Windows.Forms.Button();
+            this.btnCerrarAl = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(216)))), ((int)(((byte)(216)))));
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.lblAlDoc);
+            this.panel2.Controls.Add(this.lblCédula);
+            this.panel2.Controls.Add(this.lblContra);
+            this.panel2.Controls.Add(this.lblNombre);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.splitter1);
+            this.panel2.Controls.Add(this.btnVolver);
+            this.panel2.Location = new System.Drawing.Point(14, 129);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1238, 550);
+            this.panel2.TabIndex = 12;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Location = new System.Drawing.Point(11, 89);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(173, 120);
+            this.panel4.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "(Imagen)";
+            // 
+            // lblAlDoc
+            // 
+            this.lblAlDoc.AutoSize = true;
+            this.lblAlDoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlDoc.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblAlDoc.Location = new System.Drawing.Point(217, 221);
+            this.lblAlDoc.Name = "lblAlDoc";
+            this.lblAlDoc.Size = new System.Drawing.Size(246, 33);
+            this.lblAlDoc.TabIndex = 17;
+            this.lblAlDoc.Text = "Alumno / Docente";
+            // 
+            // lblCédula
+            // 
+            this.lblCédula.AutoSize = true;
+            this.lblCédula.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCédula.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblCédula.Location = new System.Drawing.Point(217, 132);
+            this.lblCédula.Name = "lblCédula";
+            this.lblCédula.Size = new System.Drawing.Size(235, 33);
+            this.lblCédula.TabIndex = 16;
+            this.lblCédula.Text = "Cédula: (Cédula)";
+            // 
+            // lblContra
+            // 
+            this.lblContra.AutoSize = true;
+            this.lblContra.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContra.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblContra.Location = new System.Drawing.Point(217, 176);
+            this.lblContra.Name = "lblContra";
+            this.lblContra.Size = new System.Drawing.Size(351, 33);
+            this.lblContra.TabIndex = 15;
+            this.lblContra.Text = "Contraseña: (Contraseña)";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblNombre.Location = new System.Drawing.Point(217, 89);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(392, 33);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre: (Nombre Completo)";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(101)))), ((int)(((byte)(101)))));
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Location = new System.Drawing.Point(1, 1);
+            this.panel3.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1237, 71);
+            this.panel3.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(3, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "USUARIOS";
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(201, 550);
+            this.splitter1.TabIndex = 14;
+            this.splitter1.TabStop = false;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(101)))), ((int)(((byte)(101)))));
+            this.btnVolver.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnVolver.FlatAppearance.BorderSize = 0;
+            this.btnVolver.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVolver.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVolver.ForeColor = System.Drawing.Color.White;
+            this.btnVolver.Location = new System.Drawing.Point(1107, 511);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(120, 29);
+            this.btnVolver.TabIndex = 8;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
+            this.panel1.Controls.Add(this.btnGruposAl);
+            this.panel1.Controls.Add(this.btnPerfilDoc);
+            this.panel1.Controls.Add(this.btnConsultasAl);
+            this.panel1.Controls.Add(this.btnChatAl);
+            this.panel1.Controls.Add(this.btnCerrarAl);
+            this.panel1.Location = new System.Drawing.Point(0, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1264, 122);
+            this.panel1.TabIndex = 11;
+            // 
+            // btnGruposAl
+            // 
+            this.btnGruposAl.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGruposAl.FlatAppearance.BorderSize = 0;
+            this.btnGruposAl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGruposAl.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGruposAl.Location = new System.Drawing.Point(6, 5);
+            this.btnGruposAl.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.btnGruposAl.Name = "btnGruposAl";
+            this.btnGruposAl.Size = new System.Drawing.Size(192, 112);
+            this.btnGruposAl.TabIndex = 0;
+            this.btnGruposAl.Text = "GRUPOS";
+            this.btnGruposAl.UseVisualStyleBackColor = true;
+            // 
+            // btnPerfilDoc
+            // 
+            this.btnPerfilDoc.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPerfilDoc.FlatAppearance.BorderSize = 0;
+            this.btnPerfilDoc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPerfilDoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPerfilDoc.Location = new System.Drawing.Point(1111, 5);
+            this.btnPerfilDoc.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.btnPerfilDoc.Name = "btnPerfilDoc";
+            this.btnPerfilDoc.Size = new System.Drawing.Size(141, 112);
+            this.btnPerfilDoc.TabIndex = 6;
+            this.btnPerfilDoc.Text = "Perfil";
+            this.btnPerfilDoc.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultasAl
+            // 
+            this.btnConsultasAl.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConsultasAl.FlatAppearance.BorderSize = 0;
+            this.btnConsultasAl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConsultasAl.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultasAl.Location = new System.Drawing.Point(197, 6);
+            this.btnConsultasAl.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.btnConsultasAl.Name = "btnConsultasAl";
+            this.btnConsultasAl.Size = new System.Drawing.Size(237, 112);
+            this.btnConsultasAl.TabIndex = 1;
+            this.btnConsultasAl.Text = "USUARIOS";
+            this.btnConsultasAl.UseVisualStyleBackColor = true;
+            // 
+            // btnChatAl
+            // 
+            this.btnChatAl.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnChatAl.FlatAppearance.BorderSize = 0;
+            this.btnChatAl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChatAl.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChatAl.Location = new System.Drawing.Point(427, 5);
+            this.btnChatAl.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.btnChatAl.Name = "btnChatAl";
+            this.btnChatAl.Size = new System.Drawing.Size(236, 112);
+            this.btnChatAl.TabIndex = 2;
+            this.btnChatAl.Text = "HISTORIAL";
+            this.btnChatAl.UseVisualStyleBackColor = true;
+            // 
+            // btnCerrarAl
+            // 
+            this.btnCerrarAl.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCerrarAl.FlatAppearance.BorderSize = 0;
+            this.btnCerrarAl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCerrarAl.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrarAl.Location = new System.Drawing.Point(918, 5);
+            this.btnCerrarAl.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.btnCerrarAl.Name = "btnCerrarAl";
+            this.btnCerrarAl.Size = new System.Drawing.Size(203, 112);
+            this.btnCerrarAl.TabIndex = 5;
+            this.btnCerrarAl.Text = "Cerrar sesión";
+            this.btnCerrarAl.UseVisualStyleBackColor = true;
+            // 
+            // VerUsuAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "VerUsuAdmin";
+            this.Text = "VerUsuAdmin";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnGruposAl;
+        private System.Windows.Forms.Button btnPerfilDoc;
+        private System.Windows.Forms.Button btnConsultasAl;
+        private System.Windows.Forms.Button btnChatAl;
+        private System.Windows.Forms.Button btnCerrarAl;
+        private System.Windows.Forms.Label lblAlDoc;
+        private System.Windows.Forms.Label lblCédula;
+        private System.Windows.Forms.Label lblContra;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label2;
+    }
+}
